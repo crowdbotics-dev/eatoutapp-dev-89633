71ab93e7845aa4345ea0dfb75538f0a544811fa7
Crowdbotics <engineering@crowdbotics.com>
2023-06-08 13:07:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "eatoutapp-dev-89633",
+  url: "https://eatoutapp-dev-89633.botics.co",
+  api: "https://eatoutapp-dev-89633.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
